f1d1e5516912e0d9e0e26d5ba53fcf8b52651fa3
anna maslova <zorina.a.1@yandex.ru>
2022-07-14 18:47:00 +0300

    005

---
+++ 005/Program.cs
@@ -0,0 +1,18 @@
+﻿// 5.	С клавиатуры вводятся три числа. Найти максимальное из трех чисел.
+
+Console.WriteLine("Введите на клавиатуре три числа:");
+int a,b,c;
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+s = Console.ReadLine();
+c = Convert.ToInt32(s);
+
+Console.WriteLine("Хорошо, давайте посмотрим результат:");
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
