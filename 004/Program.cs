422daee68ea76f857ea26336338b95028684e02f
anna maslova <zorina.a.1@yandex.ru>
2022-07-14 18:45:43 +0300

    004

---
+++ 004/Program.cs
@@ -0,0 +1,27 @@
+﻿// 4.	По заданному с клавиатуры номеру дня недели вывести его название.
+
+Console.WriteLine("Введите номер дня недели:");
+int a = 1; 
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+s = System.Console.ReadLine();
+b = Convert.ToInt32(s);
+
+if (s = a)
+{
+    System.Console.WriteLine("Понедельник");
+}
+if (s = b)
+{
+    System.Console.WriteLine("Вторник");
+}
+
+
+
+
