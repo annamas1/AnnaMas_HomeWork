f773ed8138f0719252fd3ad1db87343ae776d50b
anna maslova <zorina.a.1@yandex.ru>
2022-07-14 23:19:55 +0300

    004

--- 004/Program.cs
+++ 004/Program.cs
@@ -8,20 +8,43 @@ int d = 4;
 int e = 5;
 int f = 6;
 int g = 7;
-string? s = Console.ReadLine();
+string? s =  Console.ReadLine();
 a = Convert.ToInt32(s);
-s = System.Console.ReadLine();
+s = Console.ReadLine();
 b = Convert.ToInt32(s);
+c = Convert.ToInt32(s);
+d = Convert.ToInt32(s);
+e = Convert.ToInt32(s);
+f = Convert.ToInt32(s);
+g = Convert.ToInt32(s);
 
-if (s = a)
+if (s=1)
 {
     System.Console.WriteLine("Понедельник");
 }
-if (s = b)
+if (s=2)
 {
     System.Console.WriteLine("Вторник");
 }
-
-
+if (s=3)
+{
+    System.Console.WriteLine("Среда");
+}
+if (s=d)
+{
+    System.Console.WriteLine("Четверг");
+}
+if (s=e)
+{
+    System.Console.WriteLine("Пятница");
+}
+if (s=g)
+{
+    System.Console.WriteLine("Суббота");
+}
+if (s=g)
+{
+    System.Console.WriteLine("Воскрескенье");
+}
 
 
