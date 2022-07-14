dfe5dc27dc0e6cd931b89f1d917b80f284808184
anna maslova <zorina.a.1@yandex.ru>
2022-07-15 00:38:06 +0300

    015

---
+++ 015/Program.cs
@@ -0,0 +1,11 @@
+﻿// 15.	С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет.
+
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>=100 || a<=-100)
+{
+    System.Console.WriteLine(a/100%10);
+}
+else
+{
+    System.Console.WriteLine("Число имеет меньше 3х цифр");
+}
