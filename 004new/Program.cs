2fe7400d89f9d9ca3a25abc777ae9ca4e45cd810
anna maslova <zorina.a.1@yandex.ru>
2022-07-15 20:27:06 +0300

    004n

---
+++ 004new/Program.cs
@@ -0,0 +1,42 @@
+﻿// 4.	По заданному с клавиатуры номеру дня недели вывести его название.
+
+int dayOfWeek; 
+dayOfWeek = int.Parse(System.Console.RedLine());
+dayOfWeek = Convert.ToInt32(System.Console.RedLine());
+
+if (dayOfWeek == 1) System.Console.WriteLine("Monday");
+if (dayOfWeek == 2) System.Console.WriteLine("Tuesday");
+
+switch(dayOfWeek)
+{
+    case 1:
+        System.Console.WriteLine("Monday");
+        break;
+    case 2:
+        System.Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        System.Console.WriteLine("Wednesday");
+        break;
+    case 4;
+        System.Console.WriteLine("Monday");
+        break;
+}
+
+
+
+switch(dayOfWeek)
+{
+    case 1:
+        System.Console.WriteLine("Monday");
+        break;
+    case 2:
+        System.Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        System.Console.WriteLine("Wednesday");
+        break;
+    default:
+        System.Console.WriteLine("Wrong!");
+        break;
+}
