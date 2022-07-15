72b7fc47d2207d0299387c60045cc771fed8cec6
anna maslova <zorina.a.1@yandex.ru>
2022-07-15 21:28:40 +0300

    32

---
+++ 020/Program.cs
@@ -0,0 +1,14 @@
+﻿// 20.	Определить номер четверти плоскости, 
+//в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0.
+
+int AreaN(double x, double y);
+{
+    if(x>0 && y>0) return 1;
+    if(x<0 && y>0) return 2;
+    if(x<0 && y<0) return 3;
+    if(x>0 && y<0) return 4;
+    return 0;
+}
+int x = 40, y = 40;
+System.Console.WriteLine(AreaN(x,y));
+
