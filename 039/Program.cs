fa863cf00abb7f83e4193e49549c3e354d7288b7
anna maslova <zorina.a.1@yandex.ru>
2022-07-18 18:04:36 +0300

    039

---
+++ 039/Program.cs
@@ -0,0 +1,18 @@
+﻿// 39.	Найти сумму чисел одномерного массива стоящих на нечетной позиции.
+
+
+int N=4;
+int[] a=new int[N];
+Random random=new Random();//псевдослучайные числа
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(0,11);
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+
+int sum=0;
+for(int i=1;i<a.Length;i=i+2)
+    //sum+=a[i];
+    sum=sum+a[i];
+System.Console.WriteLine();
+System.Console.WriteLine(sum);
