577297765d7df746b88ec129094202fb91ae89ef
anna maslova <zorina.a.1@yandex.ru>
2022-07-18 16:28:10 +0300

    017

--- 017/Program.cs
+++ 017/Program.cs
@@ -1,9 +1,23 @@
-﻿
+﻿// 17.	Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
 
+int dayOfWeek; 
+dayOfWeek=Convert.ToInt32(Console.ReadLine());
 
-int Sum(int a, int b);
+if (dayOfWeek == 1) System.Console.WriteLine("Monday");
+if (dayOfWeek == 2) System.Console.WriteLine("Tuesday");
+if (dayOfWeek == 3) System.Console.WriteLine("Wednesday");
+if (dayOfWeek == 4) System.Console.WriteLine("Thursday");
+if (dayOfWeek == 5) System.Console.WriteLine("Friday");
+if (dayOfWeek == 6) System.Console.WriteLine("Saturday");
+if (dayOfWeek == 7) System.Console.WriteLine("Sunday");
+
+int b=Convert.ToInt32(Console.ReadLine());
+bool b==6 || b==7;
+if (b)
 {
-    return a + b;
+    System.Console.WriteLine("This is - Weekend!");
 }
-Sum(2,2);
-System.Console.WriteLine(Sum(2,4));
+else
+{
+    System.Console.WriteLine("Workday");
+}  
