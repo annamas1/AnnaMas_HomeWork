3a1a6fba1beab4c748475be71be45dfefc95eae6
anna maslova <zorina.a.1@yandex.ru>
2022-07-18 10:33:19 +0300

    004

--- 004/Program.cs
+++ 004/Program.cs
@@ -1,50 +1,12 @@
 ﻿// 4.	По заданному с клавиатуры номеру дня недели вывести его название.
 
-Console.WriteLine("Введите номер дня недели:");
-int a = 1; 
-int b = 2;
-int c = 3;
-int d = 4;
-int e = 5;
-int f = 6;
-int g = 7;
-string? s =  Console.ReadLine();
-a = Convert.ToInt32(s);
-s = Console.ReadLine();
-b = Convert.ToInt32(s);
-c = Convert.ToInt32(s);
-d = Convert.ToInt32(s);
-e = Convert.ToInt32(s);
-f = Convert.ToInt32(s);
-g = Convert.ToInt32(s);
+int dayOfWeek; 
+dayOfWeek=Convert.ToInt32(Console.ReadLine());
 
-if (s=1)
-{
-    System.Console.WriteLine("Понедельник");
-}
-if (s=2)
-{
-    System.Console.WriteLine("Вторник");
-}
-if (s=3)
-{
-    System.Console.WriteLine("Среда");
-}
-if (s=d)
-{
-    System.Console.WriteLine("Четверг");
-}
-if (s=e)
-{
-    System.Console.WriteLine("Пятница");
-}
-if (s=g)
-{
-    System.Console.WriteLine("Суббота");
-}
-if (s=g)
-{
-    System.Console.WriteLine("Воскрескенье");
-}
-
-
+if (dayOfWeek == 1) System.Console.WriteLine("Monday");
+if (dayOfWeek == 2) System.Console.WriteLine("Tuesday");
+if (dayOfWeek == 3) System.Console.WriteLine("Wednesday");
+if (dayOfWeek == 4) System.Console.WriteLine("Thursday");
+if (dayOfWeek == 5) System.Console.WriteLine("Friday");
+if (dayOfWeek == 6) System.Console.WriteLine("Saturday");
+if (dayOfWeek == 7) System.Console.WriteLine("Sunday");
