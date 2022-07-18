53e6d281ff7aa4b06a19532f69eacc5d93c04ddb
anna maslova <zorina.a.1@yandex.ru>
2022-07-18 11:15:47 +0300

    007

---
+++ 007/Program.cs
@@ -0,0 +1,7 @@
+﻿// 7.	Выяснить является ли число чётным.
+
+int a = 20;
+
+if (a/2==0) System.Console.WriteLine("Четное");
+if (a/2<=0) System.Console.WriteLine("Не четное");
+if (a/2>=0) System.Console.WriteLine("Не четное");
