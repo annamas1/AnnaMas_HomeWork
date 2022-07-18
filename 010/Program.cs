78a389c3f032eaf72b963a4f234036c4a6f6bd21
anna maslova <zorina.a.1@yandex.ru>
2022-07-18 12:55:45 +0300

    010

---
+++ 010/Program.cs
@@ -0,0 +1,7 @@
+﻿// 10.	Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a>=10) System.Console.WriteLine(a/1%10);
+
+if (a<=10) System.Console.WriteLine("Число имеет одну цифру");
