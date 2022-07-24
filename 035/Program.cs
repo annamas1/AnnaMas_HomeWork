5fd53fd0e8048ea02caf2227aa9c2b482c0e90fe
anna maslova <zorina.a.1@yandex.ru>
2022-07-24 19:41:07 +0300

    035

--- 035/Program.cs
+++ 035/Program.cs
@@ -1,1 +1,14 @@
 ﻿// 35.	Написать программу замены элементов массива на противоположные.
+
+
+int N=5;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(0,9);
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+Console.WriteLine("   OK, затем нажми на клавиатуре ENTER");
+string? s = Console.ReadLine();
+Console.WriteLine("Хорошо, давайте посмотрим результат:");
+
