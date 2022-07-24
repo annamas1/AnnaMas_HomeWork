50de2dc603c24690c69069f913a4d1e10c968dc2
anna maslova <zorina.a.1@yandex.ru>
2022-07-24 19:07:02 +0300

    037

--- 037/Program.cs
+++ 037/Program.cs
@@ -1,3 +1,32 @@
 ﻿// 37.	Задать массив, заполнить случайными положительными трёхзначными числами. 
 //Показать количество нечетных\четных чисел.
 
+int N=8;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+{
+    a[i]=random.Next(100,120);
+}
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+
+int count = a(
+    a[0],    a[1],    a[2],    a[3],    a[4],    a[5],    a[6],    a[7]
+);
+{
+    count = a/2;
+    while(count==0);
+    System.Console.WriteLine(count);
+}
+
+
+
+
+
+
+
+
+    
+      
+
