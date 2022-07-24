57b456f2fb548e8d18214150ab6778953f3c1d4e
anna maslova <zorina.a.1@yandex.ru>
2022-07-24 16:32:27 +0300

    021

--- 021/Program.cs
+++ 021/Program.cs
@@ -1,12 +1,42 @@
 ﻿// 21.	Задать номер четверти, показать диапазоны для возможных координат.
 
-int AreaN(double x, double y)
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 10;
+
+while(count < 100000)
 {
-    //Отладочная
-    //System.Console.WriteLine($"x={x} y={y}");
-    if (x>0 && y>0) return 1;
-    if (x<0 && y>0) return 2;
-    if (x<0 && y<0) return 3;
-    if (x>0 && y<0) return 4;
-    return 0;
+    int what = new Random().Next(0,3); // [0;3) 0 1 2
+    if(what == 0)
+    {
+        x = (x + xa) /2;
+        y = (y + ya) /2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    }
+    if(what == 2)
+    {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
 }
