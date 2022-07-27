393cec817ece617ee3a98616e7693eca6824d975
anna maslova <zorina.a.1@yandex.ru>
2022-07-27 19:34:12 +0300

    026

---
+++ 026/Program.cs
@@ -0,0 +1,14 @@
+﻿// 26.	Найти сумму чисел от 1 до А.
+
+int i=1;
+System.Console.Write($"{i} ");
+i=i+1;
+int A=5;
+
+while(i<=A)
+{
+    System.Console.Write($"{i} ");
+    i=i+1;
+}
+int b = i+1;
+System.Console.WriteLine($"сумма чисел = {b+(b+1)}");
