283598ae76567833848a50eddfadd9819ef5ffaa
anna maslova <zorina.a.1@yandex.ru>
2022-07-27 19:55:51 +0300

    027

---
+++ 027/Program.cs
@@ -0,0 +1,11 @@
+﻿// 27.	Возведите число А в натуральную степень B используя цикл.
+
+int i=1;
+int A=10;
+int B=8;
+
+while(i<=A)
+{
+    System.Console.WriteLine($"{i}*{B} = {Math.Pow(i,B)}");
+    i=i+1;    
+}
