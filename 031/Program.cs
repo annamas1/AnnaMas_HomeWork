5fc0c521286c6a0b84b3348d5d1bc284af9fac4a
anna maslova <zorina.a.1@yandex.ru>
2022-08-02 17:59:05 +0300

    031

---
+++ 031/Program.cs
@@ -0,0 +1,20 @@
+﻿// 31.	Показать кубы чисел, заканчивающихся на четную цифру.
+
+int i=1;
+int N=10;
+
+while(i<=N)
+{
+    System.Console.WriteLine($"{i}^3 = {i*i*i}");
+    i=i+1;
+
+}
+
+int a = i/2;
+if (a==0)
+{
+    System.Console.WriteLine($"заканчиваются на четную цифру {a}");
+    a=i/2;
+}
+
+
