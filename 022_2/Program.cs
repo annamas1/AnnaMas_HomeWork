b73cd29e92e2017569bb7645000c7ec3c1329f99
anna maslova <zorina.a.1@yandex.ru>
2022-08-02 14:48:16 +0300

    022_2

---
+++ 022_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// 22.	Программа проверяет пятизначное число на палиндромом.
+
+
+int []array;
+array=new int[21312];
+
+
+if(array[0]==array[4] && array[1]==array[3])
+{
+    System.Console.Write($"число  палиндром");
+    return;
+}
+if(array[0]!=array[4])
+{
+    System.Console.Write($"число  НЕ палиндром");
+}
+
