46e67872aceff96361c0ac770dab762add44f5e4
anna maslova <zorina.a.1@yandex.ru>
2022-08-02 15:47:16 +0300

    029

--- 029/Program.cs
+++ 029/Program.cs
@@ -1,6 +1,6 @@
 ﻿// 29.	Подсчитать сумму цифр в числе
 
-
+/*
 int[] array = {123};
 int b=array.Length;
 int sum=0;
@@ -12,3 +12,26 @@ System.Console.WriteLine(sum);
 
 System.Console.WriteLine($"сумма чисел ={b}");
 
+
+
+
+int []array;
+array=new int[21312];
+int sum=0;
+
+sum=array[0]+array[1]+array[2];
+    
+System.Console.WriteLine(sum);
+
+System.Console.WriteLine($"сумма чисел ={sum}");
+*/
+
+int []a;
+a=new int[21312];
+a[0]=2;
+a[1]=1;
+a[2]=3;
+a[3]=1;
+a[4]=2;
+int sum = a[0]+a[1]+a[2]+a[3]+a[4];
+System.Console.WriteLine(sum);
