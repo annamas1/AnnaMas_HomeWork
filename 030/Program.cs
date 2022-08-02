ce71ec2653d0d6724fbc99260230055b3721307b
anna maslova <zorina.a.1@yandex.ru>
2022-08-02 17:27:23 +0300

    030

---
+++ 030/Program.cs
@@ -0,0 +1,23 @@
+﻿// 30.	Написать программу вычисления произведения чисел от 1 до N.
+
+
+int i=1;
+System.Console.Write($"{i} ");
+i=i+1;
+int N=7;
+while(i<=N)
+{
+    System.Console.Write($"{i} ");
+    i=i+1;
+    
+}
+int result = 1;
+for(i = 1; i<=N; i++)
+    result*=i;
+System.Console.WriteLine($"сумма чисел = {result}");
+
+
+
+
+
+
