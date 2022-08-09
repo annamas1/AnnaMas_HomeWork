cfaaac7435cd4944d46b60d6ca866abb7b37a7cf
anna maslova <zorina.a.1@yandex.ru>
2022-08-09 16:48:23 +0300

    swap

---
+++ Swap/Program.cs
@@ -0,0 +1,26 @@
+﻿// Swap - метод обмена значениями
+
+void Swap(ref int a,ref int b)//reference
+{
+    int t=a;
+    a=b;
+    b=t;
+}
+
+void Calculate(int a, int b,out int sum, out int sub)
+{
+    sum=a+b;
+    sub=a-b;
+}
+
+
+int a=1;
+int b=2;
+System.Console.WriteLine($"a={a} b={b}");
+Swap(ref a,ref b);
+System.Console.WriteLine($"a={a} b={b}");
+a=4;
+b=5;
+
+int sum,sub;
+Calculate(2,2,out sum,out sub);
