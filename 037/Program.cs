9fa99ff6024b1e4c4246680baab4ff165af4f996
anna maslova <zorina.a.1@yandex.ru>
2022-08-09 18:42:50 +0300

    037

--- 037/Program.cs
+++ 037/Program.cs
@@ -1,32 +1,62 @@
-﻿// 37.	Задать массив, заполнить случайными положительными трёхзначными числами. 
-//Показать количество нечетных\четных чисел.
+﻿// 37.	Задать массив, заполнить случайными положительными трёхзначными числами. Показать сумму чисел.
+//Показать количество нечетных\четных чисел. --- не могу написать чтобы количество четных/нечетных считалось.
 
-int N=8;
+int N=5;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+{
+    a[i]=random.Next(100,200);
+}
+for(int j=0;j<a.Length;j++)
+{
+    System.Console.Write($"{a[j],4}");
+}
+
+int count=0;
+for(int i=0;i<a.Length;i++)
+{
+    if (a[i]>0)
+    {
+        count=count+a[i];
+    }
+}
+
+for(int i=0;i<a.Length;i++)
+   
+System.Console.WriteLine();
+System.Console.WriteLine($"Сумма положительных:{count}");
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+int N=5;
 int[] a=new int[N];
 Random random=new Random();
 for(int i=0;i<a.Length;i++)
 {
     a[i]=random.Next(100,120);
 }
-for(int i=0;i<a.Length;i++)
-    System.Console.Write($"{a[i],4}");
+for(int j=0;j<a.Length;j++)
+    System.Console.Write($"{a[j],4}");
 
-int count = a(
+int count = j(
     a[0],    a[1],    a[2],    a[3],    a[4],    a[5],    a[6],    a[7]
 );
 {
-    count = a/2;
+    count = a[0]/2;
     while(count==0);
     System.Console.WriteLine(count);
-}
-
-
-
-
-
-
-
-
-    
-      
+}    
+*/     
 
