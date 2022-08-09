dd71301db4104cd46807f3f6dc1f2fc5153f45f3
anna maslova <zorina.a.1@yandex.ru>
2022-08-09 21:59:00 +0300

    048

---
+++ 048/Program.cs
@@ -0,0 +1,70 @@
+﻿//48. Показать двумерный массив размером m×n заполненный целыми случайными числами
+
+int[,] arr;//объявляем массив
+arr=Create2DArray(3,4);
+
+//1 способ вернуть массив
+int[,] Create2DArray(int n,int m)
+{
+    int[,] a=new int[n,m];//создаем массив
+    Random random=new Random();
+    for(int i=0;i<a.GetLength(0);i++)//перебираем строки
+        for(int j=0;j<a.GetLength(1);j++)//перебираем столбцы
+            a[i,j]=random.Next(0,100);
+    return a;
+}
+
+//2 способ вернуть массив
+void Create2DArray2(int n,int m,out int[,] a)
+{
+    a=new int[n,m];//создаем массив
+    Random random=new Random();
+    for(int i=0;i<a.GetLength(0);i++)//перебираем строки
+        for(int j=0;j<a.GetLength(1);j++)//перебираем столбцы
+            a[i,j]=random.Next(0,100);
+
+}
+
+
+// Написать подпрограмму, которая в матрице чисел находит сумму элементов главной диагонали
+//
+//
+
+int[,] arr;//объявляем массив
+arr=Create2DArray(3,3);
+
+Print(arr);
+System.Console.WriteLine($"сумма элементов главной диагонали  {SumMain(arr)}");
+
+
+
+//1 способ вернуть массив
+int[,] Create2DArray(int n,int m)
+{
+    int[,] a=new int[n,m];//создаем массив
+    Random random=new Random();
+    for(int i=0;i<a.GetLength(0);i++)//перебираем строки
+        for(int j=0;j<a.GetLength(1);j++)//перебираем столбцы
+            a[i,j]=random.Next(0,100);
+    return a;
+}
+
+
+
+void Print(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)//перебираем строки
+    {
+            for(int j=0;j<a.GetLength(1);j++)//перебираем столбцы
+                System.Console.Write($"{a[i,j],4}");
+            System.Console.WriteLine();
+    }
+}
+
+int SumMain(int[,] arr)
+{
+int sum=0;
+for(int i=0;i<arr.GetLength(0);i++)//перебираем строки
+    sum=sum+arr[i,i];
+    return sum;
+}
---
+++ 050/Program.cs
@@ -0,0 +1,45 @@
+﻿// 50.	В двумерном массиве n×k заменить четные элементы на противоположные по значению (a[i,j]=-a[i,j])
+
+
+//В двумерном массиве n×k заменить четные элементы на 
+//противоположные по значению (a[i,j]=-a[i,j])
+
+int[,] a;
+a = Create2DArray(3, 4);
+Print(a);
+Solve (a);
+System.Console.WriteLine();
+Print(a);
+
+void Print(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)//перебираем строки
+    {
+        for (int j = 0; j < a.GetLength(1); j++)//перебираем столбцы
+            System.Console.Write($"{a[i, j],4}");
+        System.Console.WriteLine();
+    }
+}
+
+int[,] Create2DArray(int n, int m, int min = 0, int max = 100)
+{
+    int[,] a = new int[n, m];//создаем массив
+    Random random = new Random();
+    for (int i = 0; i < a.GetLength(0); i++)//перебираем строки
+        for (int j = 0; j < a.GetLength(1); j++)//перебираем столбцы
+            a[i, j] = random.Next(min, max + 1);
+    return a;
+}
+
+void Solve (int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)//перебираем строки
+    {
+        for (int j = 0; j < a.GetLength(1); j++)//перебираем столбцы
+        {
+            if (a[i,j] % 2 == 0)
+                a[i,j] = - a[i,j];
+        }
+        
+    }
+}
