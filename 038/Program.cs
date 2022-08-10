cde0e3b371abd62c3a05e2654209988db4681024
anna maslova <zorina.a.1@yandex.ru>
2022-08-10 16:08:48 +0300

    038

--- 038/Program.cs
+++ 038/Program.cs
@@ -1,2 +1,16 @@
-﻿// 38.	В одномерном массиве из 123 чисел 
-// найти количество элементов из отрезка [10,99]
+﻿// 38.	В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int N=5;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(10,99);
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i],4}");
+
+int count=0;
+for(int i=0;i<random.Next(10,99);i=i+2)
+    
+    count=count+1;
+System.Console.WriteLine();
+System.Console.WriteLine(count);
