912860947ac14e6bdce3f8303d17698ac18feffb
anna maslova <zorina.a.1@yandex.ru>
2022-08-10 15:51:54 +0300

    031

--- 031/Program.cs
+++ 031/Program.cs
@@ -2,17 +2,22 @@
 
 int i=1;
 int N=10;
+int kub = i;
 while(i<=N)
 {
     while(i<=N)
-    {
+        {
+        System.Console.WriteLine($"{i}^3 = {i*i*i}");
+        i=i+1;    
+        }
+    
+    System.Console.WriteLine($"кубы чисел, заканчивающихся на четную цифру : ");
     System.Console.WriteLine($"{i}^3 = {i*i*i}");
-    i=i+1;    
-    }
-
 }
-if(i/i==0) System.Console.WriteLine($"{i}^3 = {i*i*i}");
-i=i+1;
+
+
+
+
 
 
 
